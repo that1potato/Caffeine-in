ced3f6131591e6f9686e2f9776ae14c608e2ce16
Wotah Man <z.p.a.toto@gmail.com>
2025-06-12 10:52:58 +1000

    added MD3exp wavy bar below the caffeine text

--- Caffeine-in/app/build.gradle.kts
+++ Caffeine-in/app/build.gradle.kts
@@ -48,7 +48,7 @@ dependencies {
     implementation(libs.androidx.ui)
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
-    implementation(libs.androidx.material3)
+    implementation(libs.androidx.material3.v140alpha15)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
