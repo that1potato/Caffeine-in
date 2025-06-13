4b934ce624d62c0354d45515460f777093422afe
Wotah Man <z.p.a.toto@gmail.com>
2025-06-13 10:12:44 +1000

    changed folder structure

--- Caffeine-in/app/src/main/java/com/example/caffeine_in/MainActivity.kt
+++ Caffeine-in/app/src/main/java/com/example/caffeine_in/MainActivity.kt
@@ -4,16 +4,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.caffeine_in.ui.theme.CaffeineinTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import com.example.caffeine_in.ui.caffeinetracker.CaffeineTrackerScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,7 +15,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             MaterialTheme {
                 Surface {
-                    com.example.caffeine_in.ui.CaffeineTrackerScreen()
+                    CaffeineTrackerScreen()
                 }
             }
         }
