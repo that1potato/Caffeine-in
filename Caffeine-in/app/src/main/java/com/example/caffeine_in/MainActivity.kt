1f482f5d07fd0bf3545bd8770aafdc4831b58f50
Wotah Man <z.p.a.toto@gmail.com>
2025-06-13 18:10:18 +1000

    applied firacode font

--- Caffeine-in/app/src/main/java/com/example/caffeine_in/MainActivity.kt
+++ Caffeine-in/app/src/main/java/com/example/caffeine_in/MainActivity.kt
@@ -4,16 +4,16 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import com.example.caffeine_in.ui.caffeinetracker.CaffeineTrackerScreen
+import com.example.caffeine_in.ui.theme.CaffeineinTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         enableEdgeToEdge()
         super.onCreate(savedInstanceState)
         setContent {
-            MaterialTheme {
+            CaffeineinTheme {
                 Surface {
                     CaffeineTrackerScreen()
                 }
--- Caffeine-in/app/src/main/java/com/example/caffeine_in/ui/theme/Type.kt
+++ Caffeine-in/app/src/main/java/com/example/caffeine_in/ui/theme/Type.kt
@@ -2,14 +2,24 @@ package com.example.caffeine_in.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.caffeine_in.R
+
+val FiraCodeFontFamily = FontFamily(
+    Font(R.font.fira_code_regular),
+    Font(R.font.fira_code_medium, FontWeight.Medium),
+    Font(R.font.fira_code_bold, FontWeight.Bold),
+    Font(R.font.fira_code_semi_bold, FontWeight.SemiBold),
+    Font(R.font.fira_code_light, FontWeight.Light)
+)
 
 // Set of Material typography styles to start with
 val Typography = Typography(
     bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = FiraCodeFontFamily,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp,
         lineHeight = 24.sp,
