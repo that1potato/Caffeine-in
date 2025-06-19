577a68ad0153c1a715b1855072415d40bc7fd37e
Potato987. <z.p.a.toto@gmail.com>
2025-06-19 20:18:18 +1000

    Update CaffeineSource.kt

--- Caffeine-in/app/src/main/java/com/example/caffeine_in/data/CaffeineSource.kt
+++ Caffeine-in/app/src/main/java/com/example/caffeine_in/data/CaffeineSource.kt
@@ -4,6 +4,7 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class CaffeineSource(
+    // ensured items in the list will have non-duplicating names
     val name: String,
     val amount: Int,
 )
