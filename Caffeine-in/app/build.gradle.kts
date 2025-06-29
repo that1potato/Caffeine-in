cb564c44ca9d7547071d58dfd96d93945ba9b3c2
Potato987. <z.p.a.toto@gmail.com>
2025-06-29 11:25:46 +0800

    added settings screen

--- Caffeine-in/app/build.gradle.kts
+++ Caffeine-in/app/build.gradle.kts
@@ -54,6 +54,7 @@ dependencies {
     implementation(libs.androidx.lifecycle.viewmodel.compose)
     implementation(libs.androidx.datastore.preferences)
     implementation(libs.kotlinx.serialization.json)
+    implementation(libs.androidx.navigation.compose)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
---
+++ Caffeine-in/app/src/main/java/com/example/caffeine_in/ui/settings/SettingsScreen.kt
@@ -0,0 +1,9 @@
+package com.example.caffeine_in.ui.settings
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun SettingsScreen() {
+    Text("Settings Screen")
+}
