11319bf70245d96834a54f9a5de65fe197c24f14
Potato987. <55237095+that1potato@users.noreply.github.com>
2025-06-30 20:58:18 +0800

    Update TopBar.kt

--- Caffeine-in/app/src/main/java/com/example/caffeine_in/ui/caffeinetracker/components/TopBar.kt
+++ Caffeine-in/app/src/main/java/com/example/caffeine_in/ui/caffeinetracker/components/TopBar.kt
@@ -1,18 +1,15 @@
 package com.example.caffeine_in.ui.caffeinetracker.components
 
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.AutoGraph
 import androidx.compose.material.icons.outlined.Settings
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -25,10 +22,9 @@ fun TopBar( // TODO
     modifier: Modifier,
     navController: NavController
 ) {
-    val buttonColor = ButtonDefaults.buttonColors(containerColor = Color(0xFFECE0D1))
     val iconColor = Color(0xFF38220F)
     val iconModifier = Modifier.size(24.dp)
-    
+
     Row(
         modifier = modifier.padding(
             start = 16.dp,
@@ -38,30 +34,20 @@ fun TopBar( // TODO
         horizontalArrangement = Arrangement.Center
     ) {
         // ---- analysis ----
-        Button(
-            onClick = { /*TODO*/ },
-            colors = buttonColor,
-            contentPadding = PaddingValues(0.dp),
-            shape = CircleShape
-        ) {
+        IconButton(onClick = { /*TODO*/ }) {
             Icon(
                 imageVector = Icons.Outlined.AutoGraph,
-                contentDescription = "Log",
+                contentDescription = "Analysis",
                 tint = iconColor,
                 modifier = iconModifier
             )
         }
         Spacer(modifier = Modifier.weight(1f))
         // ---- settings ----
-        Button(
-            onClick = { navController.navigate("settings") },
-            colors = buttonColor,
-            contentPadding = PaddingValues(0.dp),
-            shape = CircleShape
-        ) {
+        IconButton(onClick = { navController.navigate("settings") }) {
             Icon(
                 imageVector = Icons.Outlined.Settings,
-                contentDescription = "Log",
+                contentDescription = "Settings",
                 tint = iconColor,
                 modifier = iconModifier
             )
