b74b0d70fcab61997f0339a91ccecbf31299e65b
Potato987. <55237095+that1potato@users.noreply.github.com>
2025-06-30 20:27:21 +0800

    separated settings top bar as a component

--- Caffeine-in/app/src/main/java/com/example/caffeine_in/ui/settings/components/SettingsComponents.kt
+++ Caffeine-in/app/src/main/java/com/example/caffeine_in/ui/settings/components/SettingsComponents.kt
@@ -5,15 +5,57 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
 import com.example.caffeine_in.data.SettingItem
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun SettingsTopBar(
+    modifier: Modifier,
+    navController: NavController
+) {
+    val textIconColor = Color(0xFF38220F)
+    val iconModifier = Modifier.size(24.dp)
+
+    TopAppBar(
+        modifier = modifier,
+        colors = TopAppBarDefaults.topAppBarColors(Color(0xFFECE0D1)),
+        title = {
+            Text(
+                text = "Settings",
+                fontWeight = FontWeight.Bold,
+                color = textIconColor
+            )
+        },
+        navigationIcon = {
+            IconButton(onClick = { navController.navigate("tracker") }) {
+                Icon(
+                    modifier = iconModifier,
+                    imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                    contentDescription = "Back",
+                    tint = textIconColor
+                )
+            }
+        }
+    )
+}
 
 @Composable
 fun SettingsRow(item: SettingItem) {
