81af969e8057cb45aae0fb49d0e18f2ecd5b8a4a
Potato987. <z.p.a.toto@gmail.com>
2025-06-30 19:18:38 +0800

    Update SettingsScreen.kt

--- Caffeine-in/app/src/main/java/com/example/caffeine_in/ui/settings/SettingsScreen.kt
+++ Caffeine-in/app/src/main/java/com/example/caffeine_in/ui/settings/SettingsScreen.kt
@@ -2,8 +2,20 @@ package com.example.caffeine_in.ui.settings
 
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
+import com.example.caffeine_in.ui.theme.CaffeineinTheme
 
 @Composable
-fun SettingsScreen() {
+fun SettingsScreen(navController: NavController) {
     Text("Settings Screen")
+}
+
+@Preview(showBackground = true, widthDp = 360, heightDp = 800)
+@Composable
+fun DefaultPreview() {
+    CaffeineinTheme {
+        SettingsScreen(navController = rememberNavController())
+    }
 }
