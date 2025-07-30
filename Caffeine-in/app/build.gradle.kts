32054b1e42cd76702f68924feafaf80e802d95e4
Wotah Man <z.p.a.toto@gmail.com>
2025-07-30 16:42:36 +1000

    Update build.gradle.kts

--- Caffeine-in/app/build.gradle.kts
+++ Caffeine-in/app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
     defaultConfig {
         applicationId = "com.example.caffeine_in"
         minSdk = 26
-        targetSdk = 35
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
 
