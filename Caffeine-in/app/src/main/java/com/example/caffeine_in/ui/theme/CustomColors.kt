6346fea47ee161a07702d90005fd6f0d42901385
Brian Zhang <u8051120@anu.edu.au>
2025-08-18 18:25:51 +1000

    Create CustomColors.kt

---
+++ Caffeine-in/app/src/main/java/com/example/caffeine_in/ui/theme/CustomColors.kt
@@ -0,0 +1,38 @@
+package com.example.caffeine_in.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+// Primary Colors
+val Background = Color(0xFFECE0D1)
+val PrimaryText = Color(0xFF38220F)
+val SecondaryText = Color(0xFF967259)
+
+// Accent Colors
+val Orange = Color(0xFFE57825)
+
+// Status Colors
+val Success = Color(0xFF8BBD78)
+val Warning = Color(0xFFF6D484)
+val Error = Color(0xFFE53935)
+
+// UI Elements
+val Divider = Color(0x5F967259)
+val CardBackground = Background
+val TopBarBackground = Background
+
+// Button Colors
+val FABBackground = Orange
+val FABContent = PrimaryText
+val EditButtonBackground = Background
+val DeleteButtonBackground = Error
+
+// TextField Colors
+val TextFieldBorder = SecondaryText
+val TextFieldFocusedBorder = SecondaryText
+val TextFieldLabel = SecondaryText
+val TextFieldFocusedLabel = PrimaryText
+val TextFieldText = PrimaryText
+val TextFieldContainer = Background
+
+
+
