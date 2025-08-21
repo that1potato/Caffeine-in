a4d6b79e5a8b9447190a4646bcae135ab06a603d
Brian Zhang <u8051120@anu.edu.au>
2025-08-21 14:22:56 +1000

    Update Destination.kt

--- Caffeine-in/app/src/main/java/com/example/caffeine_in/navigation/Destination.kt
+++ Caffeine-in/app/src/main/java/com/example/caffeine_in/navigation/Destination.kt
@@ -1,36 +1,52 @@
 package com.example.caffeine_in.navigation
 
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.rounded.BarChart
-import androidx.compose.material.icons.rounded.Coffee
-import androidx.compose.material.icons.rounded.Settings
+import androidx.compose.material.icons.outlined.Approval
+import androidx.compose.material.icons.outlined.AutoGraph
+import androidx.compose.material.icons.outlined.Coffee
+import androidx.compose.material.icons.outlined.Info
+import androidx.compose.material.icons.outlined.Settings
 import androidx.compose.ui.graphics.vector.ImageVector
 
 sealed class Destination(
     val route: String,
     val label: String,
-    val selectedIcon: ImageVector,
-    val unselectedIcon: ImageVector = selectedIcon
+    val icon: ImageVector
 ) {
     data object Tracker : Destination(
         route = "tracker",
-        label = "Track",
-        selectedIcon = Icons.Rounded.Coffee
+        label = "Tracker",
+        icon = Icons.Outlined.Coffee
     )
+    
     data object Analysis : Destination(
         route = "analysis",
         label = "Analysis",
-        selectedIcon = Icons.Rounded.BarChart
+        icon = Icons.Outlined.AutoGraph
     )
+    
     data object Settings : Destination(
         route = "settings",
         label = "Settings",
-        selectedIcon = Icons.Rounded.Settings
+        icon = Icons.Outlined.Settings
+    )
+    
+    data object Info : Destination(
+        route = "info",
+        label = "Info",
+        icon = Icons.Outlined.Info
+    )
+    
+    data object Licenses : Destination(
+        route = "licenses",
+        label = "Licenses",
+        icon = Icons.Outlined.Approval
     )
 }
 
-val bottomDestinations = listOf(
-    Destination.Tracker,
+// Navigation items in the toolbar
+val toolbarDestinations = listOf(
     Destination.Analysis,
+    Destination.Tracker,
     Destination.Settings
 )
