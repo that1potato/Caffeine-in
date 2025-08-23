a0b0c3faea202513b45a3fda69d68ed342040ede
Brian Zhang <u8051120@anu.edu.au>
2025-08-23 20:10:10 +1000

    Update IntakeHistoryEntries.kt

--- Caffeine-in/app/src/main/java/com/example/caffeine_in/ui/analysis/components/IntakeHistoryEntries.kt
+++ Caffeine-in/app/src/main/java/com/example/caffeine_in/ui/analysis/components/IntakeHistoryEntries.kt
@@ -1,23 +1,52 @@
 package com.example.caffeine_in.ui.analysis.components
 
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.caffeine_in.data.CaffeineIntake
+import com.example.caffeine_in.data.SettingItem
 import java.text.SimpleDateFormat
 import java.util.Date
 import java.util.Locale
+
+@Composable
+fun PerDaySection(
+
+) {
+    Card(
+        modifier = Modifier
+            .padding(16.dp),
+        shape = RoundedCornerShape(16.dp),
+        colors = CardDefaults.cardColors(containerColor = Color(0xFFECE0D1)),
+        border = BorderStroke(width = 1.dp, color = Color(0xFF967259))
+    ) {
+        Text("Today") // date, "today" if today
+        Column{
+            // history entries today
+        }
+    }
+}
 
 @Composable
 fun IntakeHistoryItem(
