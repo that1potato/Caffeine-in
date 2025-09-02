4688b0e750affd3cce5e296121b17c44399e693f
Brian Zhang <55237095+that1potato@users.noreply.github.com>
2025-09-02 23:16:16 +1000

    Create CaffeineBand.kt

---
+++ Caffeine-in/app/src/main/java/com/example/caffeine_in/data/CaffeineBand.kt
@@ -0,0 +1,28 @@
+package com.example.caffeine_in.data
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.CheckCircleOutline
+import androidx.compose.material.icons.filled.ErrorOutline
+import androidx.compose.material.icons.filled.WarningAmber
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+
+sealed class CaffeineBand(
+    val name: String,
+    val color: Color,
+    val icon: ImageVector
+) {
+    object Normal : CaffeineBand("Normal", Color(0xFF70B058), Icons.Filled.CheckCircleOutline)
+    object Caution : CaffeineBand("Caution", Color(0xFFF3A800), Icons.Filled.ErrorOutline)
+    object Warning : CaffeineBand("Warning", Color(0xFFE53935), Icons.Filled.WarningAmber)
+    
+    companion object {
+        fun getBand(caffeineAmount: Double): CaffeineBand {
+            return when {
+                caffeineAmount < 400 -> Normal
+                caffeineAmount in 400.0..600.0 -> Caution
+                else -> Warning
+            }
+        }
+    }
+}
