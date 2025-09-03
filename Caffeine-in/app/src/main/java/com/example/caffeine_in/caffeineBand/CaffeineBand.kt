63e5d9a57514e205212784d600d16e6702cf5ce1
Brian Zhang <55237095+that1potato@users.noreply.github.com>
2025-09-03 18:27:34 +1000

    Update CaffeineBand.kt

--- Caffeine-in/app/src/main/java/com/example/caffeine_in/caffeineBand/CaffeineBand.kt
+++ Caffeine-in/app/src/main/java/com/example/caffeine_in/caffeineBand/CaffeineBand.kt
@@ -10,11 +10,34 @@ import androidx.compose.ui.graphics.vector.ImageVector
 sealed class CaffeineBand(
     val name: String,
     val color: Color,
-    val icon: ImageVector
+    val icon: ImageVector,
+    val range: String,
+    val description: String
 ) {
-    object Normal : CaffeineBand("NORMAL", Color(0xFF70B058), Icons.Filled.CheckCircleOutline)
-    object Caution : CaffeineBand("CAUTION", Color(0xFFF3A800), Icons.Filled.ErrorOutline)
-    object Warning : CaffeineBand("WARNING", Color(0xFFE53935), Icons.Filled.WarningAmber)
+    object Normal :
+        CaffeineBand(
+            name = "NORMAL",
+            color = Color(0xFF70B058),
+            icon = Icons.Filled.CheckCircleOutline,
+            range = "≤ 400mg/24h",
+            description = ""
+        )
+    object Caution :
+        CaffeineBand(
+            name = "CAUTION",
+            color = Color(0xFFF3A800),
+            icon = Icons.Filled.ErrorOutline,
+            range = "400~600mg/24h",
+            description = ""
+        )
+    object Warning :
+        CaffeineBand(
+            name = "WARNING",
+            color = Color(0xFFE53935),
+            icon = Icons.Filled.WarningAmber,
+            range = "> 600mg/24h",
+            description = ""
+        )
     
     companion object {
         fun getBand(caffeineAmount: Double): CaffeineBand {
