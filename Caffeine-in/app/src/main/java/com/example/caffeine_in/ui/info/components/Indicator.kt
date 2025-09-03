aaec949859d27a986841e8391b64439f191ab240
Brian Zhang <55237095+that1potato@users.noreply.github.com>
2025-09-03 18:37:59 +1000

    code cleanup

--- Caffeine-in/app/src/main/java/com/example/caffeine_in/ui/info/components/Indicator.kt
+++ Caffeine-in/app/src/main/java/com/example/caffeine_in/ui/info/components/Indicator.kt
@@ -1,13 +1,14 @@
 package com.example.caffeine_in.ui.info.components
 
 import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
@@ -24,58 +25,23 @@ import com.example.caffeine_in.caffeineBand.CaffeineBand
 
 @Composable
 fun Indicator() {
-    Normal()
-    Spacer(modifier = Modifier.height(12.dp))
-    Caution()
-    Spacer(modifier = Modifier.height(12.dp))
-    Warning()
-}
-
-@Composable
-fun Normal() {
-    Card(
-        modifier = Modifier
-            .fillMaxWidth()
-            .border(
-                1.dp,
-                CaffeineBand.Normal.color,
-                RoundedCornerShape(16.dp)
-            ),
-        shape = RoundedCornerShape(16.dp),
-        colors = CardDefaults.cardColors(containerColor = Color(0xFFECE0D1))
-    ) {
-        Row(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(12.dp),
-            verticalAlignment = Alignment.CenterVertically
-        ) {
-            Column(modifier = Modifier.weight(1f)) {
-                Text(
-                    text = "Normal  ❘ ≤ 400mg/24h",
-                    fontWeight = FontWeight.SemiBold,
-                    fontSize = 16.sp,
-                    color = Color(0xFF38220F)
-                )
-            }
-            Icon(
-                imageVector = CaffeineBand.Normal.icon,
-                tint = CaffeineBand.Normal.color,
-                contentDescription = "Normal Icon",
-                modifier = Modifier.size(24.dp)
-            )
+    val caffeineBands = listOf(CaffeineBand.Normal, CaffeineBand.Caution, CaffeineBand.Warning)
+    
+    Column(verticalArrangement = Arrangement.spacedBy(12.dp) ) {
+        caffeineBands.forEach { band ->
+            IndicatorRow(band = band)
         }
     }
 }
 
 @Composable
-fun Caution() {
+fun IndicatorRow(band: CaffeineBand) {
     Card(
         modifier = Modifier
             .fillMaxWidth()
             .border(
                 1.dp,
-                CaffeineBand.Caution.color,
+                band.color,
                 RoundedCornerShape(16.dp)
             ),
         shape = RoundedCornerShape(16.dp),
@@ -87,56 +53,25 @@ fun Caution() {
                 .padding(12.dp),
             verticalAlignment = Alignment.CenterVertically
         ) {
-            Column(modifier = Modifier.weight(1f)) {
-                Text(
-                    text = "Caution ❘ 400~600mg/24h",
-                    fontWeight = FontWeight.SemiBold,
-                    fontSize = 16.sp,
-                    color = Color(0xFF38220F)
-                )
-            }
             Icon(
-                imageVector = CaffeineBand.Caution.icon,
-                tint = CaffeineBand.Caution.color,
-                contentDescription = "Normal Icon",
+                imageVector = band.icon,
+                tint = band.color,
+                contentDescription = "${band.name} Icon",
                 modifier = Modifier.size(24.dp)
             )
-        }
-    }
-}
-
-@Composable
-fun Warning() {
-    Card(
-        modifier = Modifier
-            .fillMaxWidth()
-            .border(
-                1.dp,
-                CaffeineBand.Warning.color,
-                RoundedCornerShape(16.dp)
-            ),
-        shape = RoundedCornerShape(16.dp),
-        colors = CardDefaults.cardColors(containerColor = Color(0xFFECE0D1))
-    ) {
-        Row(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(12.dp),
-            verticalAlignment = Alignment.CenterVertically
-        ) {
-            Column(modifier = Modifier.weight(1f)) {
-                Text(
-                    text = "Warning ❘ > 600mg/24h",
-                    fontWeight = FontWeight.SemiBold,
-                    fontSize = 16.sp,
-                    color = Color(0xFF38220F)
-                )
-            }
-            Icon(
-                imageVector = CaffeineBand.Warning.icon,
-                tint = CaffeineBand.Warning.color,
-                contentDescription = "Normal Icon",
-                modifier = Modifier.size(24.dp)
+            Spacer(modifier = Modifier.width(8.dp))
+            Text(
+                text = band.name,
+                fontWeight = FontWeight.SemiBold,
+                fontSize = 16.sp,
+                color = Color(0xFF38220F)
+            )
+            Spacer(modifier = Modifier.weight(1f))
+            Text(
+                text = band.range,
+                fontWeight = FontWeight.SemiBold,
+                fontSize = 16.sp,
+                color = Color(0xFF38220F)
             )
         }
     }
